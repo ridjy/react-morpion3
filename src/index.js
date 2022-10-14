7cd466d90b8c70648d229229658b15115ab96f2a
Eddy <eddy@qdata-mg.com>
2022-10-14 18:07:03 +0300

    gagant ok

--- src/index.js
+++ src/index.js
@@ -27,6 +27,9 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();//contient une copie du tableau car slice sans parametre
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({squares: squares,
       xIsNext: !this.state.xIsNext
@@ -34,7 +37,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Prochain joueur : ' + (this.state.xIsNext ? 'X' : 'O');
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = winner + ' a gagné';
+    } else {
+      status = 'Prochain joueur : ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
@@ -59,6 +68,25 @@ class Board extends React.Component {
   }
 }
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 class Game extends React.Component {
   render() {
     return (
